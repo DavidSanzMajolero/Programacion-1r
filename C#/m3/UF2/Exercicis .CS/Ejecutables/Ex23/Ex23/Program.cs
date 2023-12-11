6e2fdf4bc35162b2d08077212d77880895215bd5
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2023-12-11 12:09:28 +0100

    Ex23 CREATED

---
+++ C#/m3/UF2/Exercicis .CS/Ejecutables/Ex23/Ex23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Activitats;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce la base y luego la altura del cuadrado: ");
+            int Numbase = Convert.ToInt16(Console.ReadLine());
+            int altura = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("El area es: " + Area(Numbase, altura, false));
+            Console.WriteLine("Introduce el radio del circulo: ");
+            double radi = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("El area es: " + Area(radi));
+        }
+        static double Area (int Base, int Altura, bool isPentagon)
+        {      
+            if(isPentagon)
+            {
+                return (Base * Altura) / 2;
+            }
+            return Base * Altura;
+        }
+        static double Area (double Radi)
+        {
+            return 3.14 * (Radi * 2);
+        }
+    }
