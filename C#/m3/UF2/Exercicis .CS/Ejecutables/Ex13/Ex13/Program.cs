62ab12a878e30963764715ba9403d8e9f2526145
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2023-12-18 12:24:23 +0100

    Ex13

---
+++ C#/m3/UF2/Exercicis .CS/Ejecutables/Ex13/Ex13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Exercicis
+{
+    public class Ex13
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Introdueix 20 numeros: ");
+            NumbersArray();
+        }
+        public static void NumbersArray() 
+        {
+            const int n = 20;
+            int[] arr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++) arr[i] = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 != 0) 
+                {
+                    Console.Write("\t" + arr[i]);
+                }
+            }
+        }
+
+        
+    }
+}
