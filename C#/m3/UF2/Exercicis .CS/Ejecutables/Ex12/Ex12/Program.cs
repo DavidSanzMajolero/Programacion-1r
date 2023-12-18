4cb9e7d47eb7a5baf3c9bf8c94ccdeec4ff30f56
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2023-12-18 12:14:21 +0100

    Ex 12

--- C#/m3/UF2/Exercicis .CS/Ejecutables/Ex12/Ex12/Program.cs
+++ C#/m3/UF2/Exercicis .CS/Ejecutables/Ex12/Ex12/Program.cs
@@ -3,9 +3,34 @@ namespace Exercicis
 {
     public class Ex12
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Introdueix el numero de litres consumits: ");
+            double litres = Convert.ToInt32(Console.ReadLine());
+            double resultat = Precio(litres);
+            Console.WriteLine($"El preu és {resultat} euros");
+        }
+        public static double Precio(double litres)
+        {
+            double mem = 6;
+            double preu = 0;
+
+            if (litres < 50)  return mem;
+
+            else if (litres > 50 && litres < 200 )
+            {
+                litres -= 50;
+                preu = litres * 0.1;
+                return preu + mem;
+            }
+
+            else
+            {
+                litres -= 200;
+                mem += 15;
+                preu = litres * 0.3;                
+                return preu+mem;
+            }
         }
     }
 }
--- C#/m3/UF2/Exercicis .CS/Ejecutables/Ex23/Ex23/Program.cs
+++ C#/m3/UF2/Exercicis .CS/Ejecutables/Ex23/Ex23/Program.cs
@@ -4,24 +4,32 @@ namespace Activitats;
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Introduce la base y luego la altura del cuadrado: ");
-            int Numbase = Convert.ToInt16(Console.ReadLine());
-            int altura = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("El area es: " + Area(Numbase, altura, false));
+            Console.WriteLine("Introduce la base y luego la altura del cuadrado(i rectangulo): ");
+            double Numbase = Convert.ToInt16(Console.ReadLine());
+            double altura = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("El area del cuadrado y del rectangulo es: " + Area(Numbase, altura, false));
             Console.WriteLine("Introduce el radio del circulo: ");
             double radi = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("El area es: " + Area(radi));
+            Console.WriteLine("El area del circulo es: " + Area(radi));
+            Console.WriteLine("Introduce la apotema y luego la longitud del pentagono: ");
+            Numbase = Convert.ToInt16(Console.ReadLine());
+            altura = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("El area del pentagono es: " + Area(Numbase, altura, true));
         }
-        static double Area (int Base, int Altura, bool isPentagon)
+        static double Area (double Base, double Altura, bool isPentagon)
         {      
-            if(isPentagon)
+            if (isPentagon)
             {
                 return (Base * Altura) / 2;
             }
-            return Base * Altura;
+            else
+            {
+                return Base * Altura;
+            }
         }
         static double Area (double Radi)
         {
             return 3.14 * (Radi * 2);
         }
     }
+
