ce6c670a0277a5f52a4d5bbc56d732e0f1e63185
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2023-12-18 13:15:33 +0100

    Ex15

---
+++ C#/m3/UF2/Exercicis .CS/Ejecutables/Ex15/Ex15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Exercicis
+{
+    public class Ex15
+    {
+        public static void Main()
+        {
+            Console.Write("Introdueix un any: ");
+            int any = Convert.ToInt32(Console.ReadLine());
+            EsBisiesto(any);
+
+            
+        }
+        public static void EsBisiesto(int any)
+        {
+            if ((any % 4 == 0 && any % 100 != 0) || (any % 400 == 0))
+            {
+                Console.WriteLine($"{any} és un any de traspàs.");
+            }
+            else
+            {
+                Console.WriteLine($"{any} no és un any de traspàs.");
+            }
+
+        }
+    }
+}
